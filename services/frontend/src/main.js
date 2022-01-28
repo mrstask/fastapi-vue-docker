c3944778a5875f462e2ae589be74515f48cd984c
lazarenko <lazarenko@google.com>
2022-01-28 16:55:56 +0200

    init

---
+++ services/frontend/src/main.js
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios';
+import Vue from 'vue';
+
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:5001/';  // the FastAPI backend
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app');
